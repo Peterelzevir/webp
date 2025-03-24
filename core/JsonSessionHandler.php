5a83389e2f9fd6eb96904d8fcf9598b519cf8579
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-24 10:27:01 +0700

    Update JsonSessionHandler.php

--- core/JsonSessionHandler.php
+++ core/JsonSessionHandler.php
@@ -314,7 +314,7 @@ class JsonSessionHandler implements SessionHandlerInterface
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
             curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 20); // 5 second timeout to prevent hanging
+            curl_setopt($ch, CURLOPT_TIMEOUT, 50); // 5 second timeout to prevent hanging
             
             $output = curl_exec($ch);
             $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);

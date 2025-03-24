4bd6b726b5f7eb8695da13f2895177092589f475
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-24 10:27:32 +0700

    Update form.php

--- core/form.php
+++ core/form.php
@@ -89,7 +89,7 @@ try {
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Increased timeout for reliability
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60); // Increased timeout for reliability
     
     $output = curl_exec($ch);
     $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);

035a13dc283a9659b3c86e148a7af526ddb4580d
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-24 08:17:06 +0700

    Update telegram_config.php

--- core/telegram_config.php
+++ core/telegram_config.php
@@ -6,5 +6,7 @@ $telegram_api_hash = "ab789476abc75fb010bda8dbc484a237";
 // Konfigurasi server Flask
 $flask_server_url = "http://127.0.0.1:5000";
 
+//ganti dengan url backend yg sesuai
+
 // Jika server Flask berada di server yang berbeda, gunakan URL lengkap
 // $flask_server_url = "http://example.com:5000";
